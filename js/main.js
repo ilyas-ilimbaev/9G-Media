46206c69de65fb455fc0d4f6be74078408673445
Ильяс Илимбаев <ilyas.ilimbaev@yandex.ru>
2022-03-09 01:52:33 +0500

    Animation circle add

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let services = document.querySelectorAll('.services-list-item');
+let about = document.querySelectorAll('.about-content-item');
+
+
+for (let elm of services) {
+  observer.observe(elm);
+}
+for (let elm of about) {
+  observer.observe(elm);
+}
