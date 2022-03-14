643defee8cb3d1caab1b6366a33fbdcf35000cb8
Ильяс Илимбаев <ilyas.ilimbaev@yandex.ru>
2022-03-14 22:41:25 +0500

    Mobile menu layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+const toggleMenu = document.querySelector('.toggle-menu');
+const mobileMenu = document.querySelector('.mobile-menu-wrapper');
+const overlay = document.querySelector('#overlay');
+
+let options = {threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let services = document.querySelectorAll('.services-list-item');
+let about = document.querySelectorAll('.about-content-item');
+
 function onEntry(entry) {
   entry.forEach(change => {
     if (change.isIntersecting) {
@@ -6,15 +15,34 @@ function onEntry(entry) {
   });
 }
 
-let options = {threshold: [0.5] };
-let observer = new IntersectionObserver(onEntry, options);
-let services = document.querySelectorAll('.services-list-item');
-let about = document.querySelectorAll('.about-content-item');
-
-
 for (let elm of services) {
   observer.observe(elm);
 }
 for (let elm of about) {
   observer.observe(elm);
 }
+//прослушиваем событие клик по гамбургеру
+toggleMenu.addEventListener('click', function() {
+    this.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
+    bodyEl.classList.toggle('noscroll');
+});
+
+//прослушиваем событие клик моб меню
+mobileMenu.addEventListener('click', function() {
+    this.classList.remove('active');
+    toggleMenu.classList.remove('active');
+    overlay.classList.remove('active');
+    bodyEl.classList.remove('noscroll');
+});
+
+//прослушиваем событие клик по overlay
+overlay.addEventListener('click', function(){
+    this.classList.remove('active');
+    toggleMenu.classList.remove('active');
+    mobileMenu.classList.toggle('active');
+    bodyEl.classList.toggle('noscroll');
+
+});
+
